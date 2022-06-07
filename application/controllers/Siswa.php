b2a9df09947c7c38ed0fec385b7397ac646884d4
Diamsyah M Dida <didadiamsyah02@gmail.com>
2022-06-07 19:47:14 +0700

    update tugas meet 11

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Siswa extends CI_Controller {
+
+  function __construct()
+  {
+    parent:: __construct();
+    $this->load->library('form_validation');
+  }
+
 	public function index()
 	{
 		$this->load->view('siswa/v-form-siswa');
@@ -9,7 +16,65 @@ class Siswa extends CI_Controller {
 
   public function save()
   {
-    if($this->validate() == true) {
+    $this->form_validation->set_rules(
+      'nis',
+      'NIS',
+      [
+        'required' => 'NIS Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'nama',
+      'Nama Siswa',
+      [
+        'required' => 'Nama Siswa Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'kelas',
+      'Kelas',
+      [
+        'required' => 'Kelas Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'tempat_lahir',
+      'Tempat Lahir',
+      [
+        'required' => 'Tempat Lahir Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'tanggal_lahir',
+      'Tanggal Lahir',
+      [
+        'required' => 'Tanggal Lahir Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'alamat',
+      'Alamat',
+      [
+        'required' => 'Alamat Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'jk',
+      'Jenis Kelamin',
+      [
+        'required' => 'Jenis Kelamin Harus diisi',
+      ]
+    );
+    $this->form_validation->set_rules(
+      'agama',
+      'Agama',
+      [
+        'required' => 'Agama Harus diisi',
+      ]
+    );
+    if($this->form_validation->run() !== true) {
+      $this->load->view('siswa/v-form-siswa');
+    } else {
       $data['data'] = $_POST;
       $this->load->view('siswa/v-index', $data);
     }
